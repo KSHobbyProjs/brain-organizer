84b832ae9b5f0109de04d115b35180db69da302b
KSHobbyProjs <111722782+KSHobbyProjs@users.noreply.github.com>
2026-06-27 18:27:05 -0400

    bug fixes and tests

--- temp_driver.py
+++ temp_driver.py
@@ -1,4 +1,7 @@
 from src.parser import KeepParser
+from src.embeddings import Embedder
+
+from sentence_transformers import SentenceTransformer
 
 parser = KeepParser("tests/Takeout/Keep")
 
@@ -7,6 +10,13 @@ notes = parser.create_notes()
 
 print(f"Loaded {len(notes)} notes.")
 print()
-
 print(notes[0])
+print()
 
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+embedder = Embedder(model)
+embeddings = embedder.embed_many([note.to_text() for note in notes])
+
+print(f"Loaded embeddings. Shape {embeddings.shape}.")
+print()
+print(embeddings[0])
