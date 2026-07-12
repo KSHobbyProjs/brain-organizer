e0cfa7edad90a80f2af5132183c79032251dfab4
KSHobbyProjs <111722782+KSHobbyProjs@users.noreply.github.com>
2026-07-12 10:03:47 -0400

    Added smart chunking and started working on graph capabilities

--- src/search.py
+++ src/search.py
@@ -1,44 +1,12 @@
 # semantic_search.py
-# A module for using pre-trained sentence transformers to search for semantic
-# meaning among embedded text. Takes as input embedded text and returns the 
-# domain object (Note) that most closely aligns with it.
-
-# EDIT: Since changed to no longer depend on Note (coupling made troubleshooting difficult)
+# A module to search an embedding space for embeddings most similar to a query
+# using cosine similarity.
+# currently acts mainly as a wrapper around sklearn kNN.
 
 from dataclasses import dataclass
 import numpy as np
 
-
-def cosine_similarity(query: np.ndarray, embeddings: np.ndarray) -> np.ndarray:
-    """
-    Compute cosine similarity between a query vector and a matrix of embeddings.
-
-    Parameters
-    ------
-    query : np.ndarray
-        Shape (d,). Embedded query vector.
-    embeddings : np.ndarray
-        Shape (n, d). Matrix of n embedding vectors.
-    
-    Returns
-    ------
-    np.ndarray
-        Shape (n,). Cosine similarity between query and each embedding.
-
-    Notes
-    -----
-    Cosine similarity is defined as:
-        sim(a, b) = (a . b) / (||a|| ||b||)
-
-    This implementation assumes inputs are not necessarily normalized.
-    """
-    # normalize query and embeddings
-    query = query / np.linalg.norm(query)
-    embeddings = embeddings / np.linalg.norm(embeddings, axis=1, keepdims=True)
-
-    # compute dot product of normalized query and embedding vector
-    scores = embeddings @ query
-    return scores
+from sklearn.neighbors import NearestNeighbors
 
 @dataclass
 class SearchResult:
@@ -49,7 +17,7 @@ class SearchResult:
 class SemanticSearcher: 
     def __init__(self, embeddings: np.ndarray):
         """
-        Takes a list of embeddings, finds the top closest embeddings
+        Takes a list of embeddings, finds the top embeddings
         closest to query embedding.
         
         Parameters
@@ -59,9 +27,48 @@ class SemanticSearcher:
         """
 
         self.embeddings = embeddings
+        self.nn = NearestNeighbors(metric='cosine').fit(embeddings)
 
+    def search(self, query: np.ndarray, k: int=5) -> list[SearchResult]:
+        """
+        Finds the top k embeddings closest to the query embedding based on some metric (cosine).
+
+        Parameters
+        ----------
+        query : np.ndarray
+            Embedded query text. Shape (d,) for d features.
+        k : int
+            Number of neighbors to find.
+
+        Results
+        -------
+        retrieved_results : list[SearchResult]
+            List of k nearest neighbors and the distance to them.
+
+        Notes
+        -----
+        This function assumes that query is a single embedded vector.
+        """
+        # compute k nearest neighbors (sklearn expects a list of queries, so reshape query)
+        distances, top_k_idx = self.nn.kneighbors(query.reshape(1, -1), n_neighbors=k, return_distance=True)
+        scores = 1 - distances.squeeze()   # sklearn outputs 1 - cos(theta) for distance. score is cos(theta)
+        top_k_idx = top_k_idx.squeeze()
+        
+        retrieved_results = [
+                SearchResult(
+                    embedding_idx=idx,
+                    score=scores[i],
+                    )
+                for i, idx in enumerate(top_k_idx)
+                ]
+        return retrieved_results
+
+
+    """ DEPRECATED """
+    """
     def search(self, query: np.ndarray, k: int=1) -> list[SearchResult]:
-        scores = cosine_similarity(query, self.embeddings)
+        from .utils import cosine_similarity_to_group
+        scores = cosine_similarity_to_group(query, self.embeddings)
         top_k_idx = np.argsort(scores)[::-1][:k]
 
         retrieved_results = [
@@ -74,4 +81,4 @@ class SemanticSearcher:
                 ]
 
         return retrieved_results
-        
+    """
---
+++ src/utils.py
@@ -0,0 +1,85 @@
+# utils.py
+# util methods shared by many classes
+
+"""
+These functions are no longer used throughout the program. sklearn already has methods which
+circumvent the need for these methods.
+
+These are kept here for posterity.
+"""
+
+import numpy as np
+
+def cosine_similarity(a: np.ndarray, b: np.ndarray) -> float:
+    """
+    Compute cosine similarity between two embeddings a and b
+
+    Parameters
+    ----------
+    a : np.ndarray
+    b : np.ndarray
+        The embeddings which to compare.
+
+    Returns
+    -------
+    similarity : float
+        The cosine similarity between embeddings a and b.
+    """
+    # normalize
+    a, b = a/np.linalg.norm(a), b/np.linalg.norm(b)
+    similarity = a @ b
+    return similarity
+
+def cosine_similarity_to_group(query: np.ndarray, embeddings: np.ndarray) -> np.ndarray:
+    """
+    Compute cosine similarity between a query vector and a group of embeddings.
+
+    Parameters
+    ------
+    query : np.ndarray
+        Shape (d,). Embedded query vector.
+    embeddings : np.ndarray
+        Shape (n, d). Matrix of n embedding vectors.
+    
+    Returns
+    ------
+    np.ndarray
+        Shape (n,). Cosine similarity between query and each embedding.
+
+    Notes
+    -----
+    Cosine similarity is defined as:
+        sim(a, b) = (a . b) / (||a|| ||b||)
+
+    This implementation assumes inputs are not necessarily normalized.
+    """
+    # normalize query and embeddings
+    query = query / np.linalg.norm(query)
+    embeddings = embeddings / np.linalg.norm(embeddings, axis=1, keepdims=True)
+
+    # compute dot product of normalized query and embedding vector
+    scores = embeddings @ query
+    return scores
+
+def cosine_similarity_mat(embeddings: np.ndarray) -> np.ndarray:
+    """
+    Compute the cosine similarity between all pairs of embeddings.
+
+    Parameters
+    ----------
+    embeddings : np.ndarray
+        Array of embeddings. Shape (n, d) for n embeddings of dimension d.
+        
+    Returns
+    -------
+    scores : np.ndarray
+        Array of similarity scores between all embeddings. Shape (n, n).
+    """
+    # normalize all embeddings
+    embeddings = embeddings / np.linalg.norm(embeddings, axis=1, keepdims=True)
+
+    # compute E @ E.T (memory hog oink oink)
+    # scores is symmetric, but E @ E.T is still faster than manual looping 
+    scores = embeddings @ embeddings.T
+    return scores
+
